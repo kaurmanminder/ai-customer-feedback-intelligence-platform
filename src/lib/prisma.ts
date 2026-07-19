9f5158ed37852ef1b19e68f28654eb9ad8b3ba3e
Manminder Kaur <kaurmanminder330@gmail.com>
2026-07-19 18:51:42 +0530

    Force fix prisma production database url

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,31 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+// Force push to reset vercel cache build
+
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 };
 
 declare global {
